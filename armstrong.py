aea82163c6df093c65910f46870e8320f3146c7b
Snehitkale <43983345+Snehitkale@users.noreply.github.com>
2018-11-27 13:30:41 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+def arm(x) :
+    sum=0
+    t=x
+    while(t>0):
+         d=t%10
+ 	 sum+=d**3
+ 	 t=t//10
+    if sum==x:
+       print("armstrong number")
+    else:
+       print("not a armstrong number")	
+
